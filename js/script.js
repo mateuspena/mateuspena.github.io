d0247eb9e31fd0c970b3cbe601f9d73502119a50
Mateus Pena <mateus.18j@gmail.com>
2020-08-01 16:53:34 -0300

    updating page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 function isMobileDevice() {
     return typeof window.orientation !== 'undefined';
 }
+
+function getIdade() {
+    const birthday = new Date('1995-09-03');
+    return (new Number((new Date().getTime() - birthday.getTime()) / 31536000000)).toFixed(0);
+}
