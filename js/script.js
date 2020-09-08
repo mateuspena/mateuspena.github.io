59d3ad86cc5708897557a1a001180767738eea52
Mateus Pena <api@focomultimidia.com>
2020-09-08 09:49:27 -0300

    modificando residencia e idade.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
 function isMobileDevice() {
     return typeof window.orientation !== 'undefined';
 }
-
-function getIdade() {
-    const birthday = new Date('1995-09-03');
-    return (new Number((new Date().getTime() - birthday.getTime()) / 31536000000)).toFixed(0);
-}
